2cce51f8c266167b8141a7b467e6442c74b32af3
Andrew Edvalson <andy.edvalson@infotechfl.com>
2016-09-24 01:16:20 -0600

    assessment editor WIP

--- app/src/main/java/com/aedvalson/classtracker/AssessmentEditorActivity.java
+++ app/src/main/java/com/aedvalson/classtracker/AssessmentEditorActivity.java
@@ -1,5 +1,6 @@
 package com.aedvalson.classtracker;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class AssessmentEditorActivity extends AppCompatActivity {
+
+    private _Assessment assessment;
+    private Uri assessmentUri;
+    private Uri courseUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,18 @@ public class AssessmentEditorActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        assessmentUri = getIntent().getParcelableExtra(DataProvider.ASSESSMENT_CONTENT_TYPE);
+        if (assessmentUri == null) {
+            // new Assessment
+            courseUri = getIntent().getParcelableExtra(DataProvider.COURSE_CONTENT_TYPE);
+        }
+        else {
+            Long assessmentId = Long.parseLong(assessmentUri.getLastPathSegment());
+            assessment = DataManager.getAssessment(this, assessmentId);
+            
+        }
+
     }
 
 }
